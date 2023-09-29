8f324c2777b50e892b03f36c181372df5ff46377
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2023-09-29 12:17:42 +0200

    test

--- api/proxy.ts
+++ api/proxy.ts
@@ -1,3 +1,12 @@
 const httpProxy = require('http-proxy');
  
-httpProxy.createProxyServer({target:'https://google.com'}).listen(8000);
+httpProxy.createProxyServer({target:'https://google.com'});
+
+export default function handler(req, res) {
+  // res.statusCode = 200;
+  // res.setHeader('Content-Type', 'application/json');
+  // res.json({ name: 'John Doe' });
+
+  // pipe the request to the proxy
+  req.pipe(httpProxy).pipe(res);
+}
